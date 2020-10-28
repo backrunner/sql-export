d4f5e8c70dd9d0966a642a97f163b26d4c007bf6
BackRunner <dev@backrunner.top>
2020-10-28 16:58:12 +0800

    fix: wrong filter

--- src/requestFilter.js
+++ src/requestFilter.js
@@ -2,6 +2,8 @@ import { getUserIp, jsonError } from './utils';
 import logger from './logger';
 import config from '../config';
 
+const filter = ['::ffff:127.0.0.1', '127.0.0.1', '::1', '::ffff'];
+
 const fn = async (ctx, next) => {
   const ip = getUserIp(ctx);
   if (!ip) {
@@ -9,7 +11,7 @@ const fn = async (ctx, next) => {
     logger.warn(`Cannot identify the request to [${ctx.url}].`)
     return;
   }
-  if (config && config.localOnly && ip !== 'localhost' && ip !== '::1') {
+  if (config && config.localOnly && !filter.includes(ip)) {
     ctx.body = jsonError('Request has been rejected.');
     logger.warn(`Request from [${ip}] to [${ctx.url}] has been rejected.`);
     return;
